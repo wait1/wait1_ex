b7f6c37b9e4c2520a81fba4d8329160d4af3f432
camshaft <bytheway.cameron@gmail.com>
2015-07-10 01:47:11 -0600

    add package info

--- mix.exs
+++ mix.exs
@@ -5,6 +5,8 @@ defmodule PlugWait1.Mixfile do
     [app: :plug_wait1,
      version: "0.1.0",
      elixir: "~> 1.0",
+     description: "Plug adapter for the wait1 protocol",
+     package: package,
      deps: deps]
   end
 
@@ -17,5 +19,12 @@ defmodule PlugWait1.Mixfile do
      { :plug, "~> 0.13.0" },
      { :poison, "~> 1.3.1" },
      { :websocket_client, github: "jeremyong/websocket_client", only: [:test]}]
+  end
+
+  defp package do
+    [files: ["lib", "mix.exs", "README*"],
+     contributors: ["Cameron Bytheway"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/wait1/plug_wait1"}]
   end
 end
